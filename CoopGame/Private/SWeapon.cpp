a53f64f07aea0e4b72d07e4669a7eb81959cef0c
unknown <kimdoy17@gmail.com>
2019-10-01 22:19:07 -0700

    Added skeletalmesh to weapon and a socket to the character

--- CoopGame/Private/SWeapon.cpp
+++ CoopGame/Private/SWeapon.cpp
@@ -9,6 +9,7 @@ ASWeapon::ASWeapon()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComp"));
 }
 
 // Called when the game starts or when spawned
--- CoopGame/Public/SWeapon.h
+++ CoopGame/Public/SWeapon.h
@@ -19,6 +19,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class USkeletalMeshComponent* MeshComp;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;

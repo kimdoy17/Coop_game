e65f79fc71732966529b58e53cfa5bc363cb2eee
unknown <kimdoy17@gmail.com>
2019-10-02 10:50:12 -0700

    Added line tracing and can now shoot

--- CoopGame/Private/SWeapon.cpp
+++ CoopGame/Private/SWeapon.cpp
@@ -2,6 +2,7 @@
 
 
 #include "SWeapon.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 ASWeapon::ASWeapon()
@@ -17,6 +18,33 @@ void ASWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void ASWeapon::Fire()
+{
+	// Trace the world, from pawn eyes to crosshair location
+
+	AActor* MyOwner = GetOwner();
+	if (MyOwner) {
+		FVector EyeLocation;
+		FRotator EyeRotation;
+		MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+		FVector TraceEnd = EyeLocation + (EyeRotation.Vector() * 10000);
+
+		// Trace against each individual triangle we hit. Good to determining head shots
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(MyOwner);
+		QueryParams.AddIgnoredActor(this);
+		QueryParams.bTraceComplex = true;
+
+		FHitResult Hit;
+		if (GetWorld()->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, ECC_Visibility, QueryParams)) {
+			// Blocking hit! Process damage
+
+		}
+		DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+	}
 }
 
 // Called every frame

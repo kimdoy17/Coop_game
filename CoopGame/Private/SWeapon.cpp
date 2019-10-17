d0455eb98c09a68112114c631091b1fb260cb62b
unknown <kimdoy17@gmail.com>
2019-10-17 08:59:29 -0700

    Added my own collision channel

--- CoopGame/Private/SWeapon.cpp
+++ CoopGame/Private/SWeapon.cpp
@@ -50,7 +50,7 @@ void ASWeapon::Fire()
 		FVector TracerEndPoint = TraceEnd;
 
 		FHitResult Hit;
-		if (GetWorld()->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, ECC_Visibility, QueryParams)) {
+		if (GetWorld()->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, COLLISION_WEAPON, QueryParams)) {
 			// Blocking hit! Process damage
 			AActor* HitActor = Hit.GetActor();
 			UGameplayStatics::ApplyPointDamage(HitActor, 20.0f, ShotDirection, Hit, MyOwner->GetInstigatorController(), this, DamageType);
